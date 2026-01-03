8809c92325bee931aab483f304abeafd9f60b8d2
Arsh Ansari <arsalanansari8030@gmail.com>
2026-01-03 21:08:27 +0530

    razorpay add

--- app/api/create-order/route.js
+++ app/api/create-order/route.js
@@ -1,15 +1,15 @@
 import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
-
 export async function POST(req) {
   const { amount } = await req.json();
 
+  const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+
   const order = await razorpay.orders.create({
-    amount,
+    amount: amount * 100, // Razorpay expects amount in paise
     currency: "INR",
   });
 
